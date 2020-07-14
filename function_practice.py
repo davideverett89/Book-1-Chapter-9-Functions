a7c70235ab56d60ed7cb44e7ddaf84a1802ba457
David Everett <davideverett1989@gmail.com>
2020-07-14 15:03:40 -0500

    Kids activities done.

--- function_practice.py
+++ function_practice.py
@@ -1,16 +1,14 @@
 # Chicken Monkey Exercise
 def chicken_monkey(range_of_nums):
-    statement = ''
     for num in range_of_nums:
         if num % 7 == 0 and num % 5 == 0:
-            statement += f"{num}: ChickenMonkey, "
+            print(num, "ChickenMonkey")
         elif num % 5 == 0:
-            statement += f"{num}: Chicken, "
+            print(num, "Chicken")
         elif num % 7 == 0:
-            statement += f"{num}: Monkey, "
-    return statement
+            print(num, "Monkey")
 
-print(chicken_monkey(range(1, 100)))
+chicken_monkey(range(1, 100))
 
 # Activities for Kids Exercise
 
@@ -19,3 +17,24 @@ print(chicken_monkey(range(1, 100)))
 # Swinging kids: Marybeth, Ariel, Kevin, Courtney
 # Sliding kids: Mike, Jack, Jennifer, Earl
 # Hiding kids: Henry, Heather, Hayley, Hugh
+
+def run(*kids):
+    for kid in kids:
+        print(f'{kid} ran like a bat out of hell!')
+
+def swing(*kids):
+    for kid in kids:
+        print(f'{kid} swung really high!')
+
+def slide(*kids):
+    for kid in kids:
+        print(f'{kid} slid really fast!')
+
+def hide_and_seek(*kids):
+    for kid in kids:
+        print(f'{kid} hid from the cops!')
+
+run("Pam", "Sam", "Andrea", "Will")
+swing("Marybeth", "Ariel", "Kevin", "Courtney")
+slide("Mike", "Jack", "Jennifer", "Earl")
+hide_and_seek("Henry", "Heather", "Hayley", "Hugh") 
