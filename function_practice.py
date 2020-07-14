4430f8be0b5b127db1e93ecd97316782f9eb4e39
David Everett <davideverett1989@gmail.com>
2020-07-14 14:50:46 -0500

    Made chickenmonkey an actual function.

--- function_practice.py
+++ function_practice.py
@@ -1,9 +1,21 @@
-nums_1_to_100 = range(1, 100)
+# Chicken Monkey Exercise
+def chicken_monkey(range_of_nums):
+    statement = ''
+    for num in range_of_nums:
+        if num % 7 == 0 and num % 5 == 0:
+            statement += f"{num}: ChickenMonkey, "
+        elif num % 5 == 0:
+            statement += f"{num}: Chicken, "
+        elif num % 7 == 0:
+            statement += f"{num}: Monkey, "
+    return statement
 
-for num in nums_1_to_100:
-    if num % 7 == 0 and num % 5 == 0:
-        print(num, 'ChickenMonkey')
-    elif num % 5 == 0:
-        print(num, 'Chicken')
-    elif num % 7 == 0:
-        print(num, 'Monkey')
+print(chicken_monkey(range(1, 100)))
+
+# Activities for Kids Exercise
+
+
+# Running kids: Pam, Sam, Andrea, Will
+# Swinging kids: Marybeth, Ariel, Kevin, Courtney
+# Sliding kids: Mike, Jack, Jennifer, Earl
+# Hiding kids: Henry, Heather, Hayley, Hugh
